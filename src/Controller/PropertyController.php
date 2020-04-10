daaf865f30561800fcb0d7bddd03cc3b8acd534c
phoenix23000 <assed2369@gmail.com>
2020-04-10 15:26:03 +0200

    first

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PropertyController extends AbstractController
+{   
+   /**
+    * @var PropertyRepository 
+    */
+    private $repository;
+
+    /**
+    * @var EntityManagerInterface
+    */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /** 
+    * @Route("/biens", name="property.index") 
+    * @return Response
+    */
+    public function index(): Response
+    { 
+        $property = $this->repository->findAllVisible();
+        $property[0]->setSold(true);
+        $this->em->flush();
+        return $this->render('property/index.html.twig',[
+            'current_menu' => 'properties'
+        ]);
+    }
+
+    /** 
+    * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug":"[a-z0-9\-]*"}) 
+    * @return Response
+    */
+    public function show(Property $property, string $slug): Response
+    { 
+        if ($property->getSlug() !== $slug )
+        {
+            return $this->redirectToRoute('property.show',[
+                'id'=> $property->getId(),
+                'slug'=> $property->getSlug()
+
+            ], 301);
+        }
+        return $this->render('property/show.html.twig',[
+            'property' => $property,
+            'current_menu' => 'properties'
+        ]);
+    }
+
+
+}
